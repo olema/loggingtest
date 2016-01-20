ea95b401c2a233584387308be7a25934b7e781c5
Oleg Matushkin <matushkin.oleg@gmail.com>
2016-01-20 14:03:37 +0300

    Рабочий пример RotatingFileHandler

--- loggingtest.py
+++ loggingtest.py
@@ -6,7 +6,21 @@
 
 import logging
 import logging.handlers
+import sys
 
+dateFormat = '%a, %d %b %Y %H:%M:S'
+fileFormat = '[%(asctime)s %(process)d] %(levelname)s (%(module)s:%(lineno)d %(message)s)'
+streamFormat = '%(asctime)s %(levelname)-8s %(message)s'
+filename = 'test.log'
 
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.DEBUG)
+fileHandler = logging.handlers.RotatingFileHandler(filename,
+                                                    maxBytes=1024, backupCount=5)
+fileHandler.setFormatter(logging.Formatter(fileFormat, dateFormat))
+rootLogger.addHandler(fileHandler)
+streamHandler = logging.StreamHandler(sys.stderr)
+streamHandler.setFormatter(logging.Formatter(streamFormat, dateFormat))
+rootLogger.addHandler(streamHandler)
 
-
+rootLogger.info("Hello! I'm rootLogger message!")
